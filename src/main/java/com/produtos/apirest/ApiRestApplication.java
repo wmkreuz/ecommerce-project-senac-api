452f2724cf7c36a79b51c64dbbc3ceb0e2d365fb
Willhan Kreuz <willhan.kreuz@santerra.local>
2023-07-05 08:55:01 -0300

    Finalização API

--- src/main/java/com/produtos/apirest/ApiRestApplication.java
+++ src/main/java/com/produtos/apirest/ApiRestApplication.java
@@ -1,10 +1,15 @@
 package com.produtos.apirest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class ApiRestApplication {
+@EnableAutoConfiguration
+@ComponentScan
+public class ApiRestApplication extends SpringBootServletInitializer {
 
 	 static public void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
